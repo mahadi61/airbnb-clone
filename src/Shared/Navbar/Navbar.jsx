baff43aa3a37a109df24dab8fc3785d311c90d35
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-08-27 00:19:49 +0600

    logo problem slove

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -11,14 +11,15 @@ const Navbar = () => {
           <Container>
             <div className="flex flex-row  items-center justify-between gap-3 md:gap-0">
               {/* logo here */}
-              <Link to="/">
+              <Link to="/" className="flex items-center">
                 <img
                   className="hidden md:block"
-                  src="./logo.png"
+                  src="./favicon.png"
                   alt="logo"
-                  width="100"
-                  height="100"
+                  width="45"
+                  height="45"
                 />
+                <p className="text-2xl font-bold text-[#FF385C]">airbnb</p>
               </Link>
               <Search />
               <MenuDropDown />
