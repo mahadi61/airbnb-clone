5db1d845d25f30fc9e33b52956f20684b45acb8c
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-08-26 22:24:39 +0600

    rooms data loaded

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Categories from "../../Components/Categories/Categories";
+import Rooms from "../../Components/Rooms/Rooms";
 
 const Home = () => {
   return (
     <div>
       <Categories />
+      <Rooms />
     </div>
   );
 };
