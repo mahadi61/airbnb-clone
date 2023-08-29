7620bef11218e4d527de4197326e6c0cdf6d37d0
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-08-29 12:30:14 +0600

    guest card increase done

--- src/Components/GuestCard/GuestCard.jsx
+++ src/Components/GuestCard/GuestCard.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const GuestCard = () => {
+  const [adults, setAdult] = useState(0);
+  const [children, setChildren] = useState(0);
+  const [infants, setInfants] = useState(0);
+
   return (
     <div className="bg-white rounded-3xl shadow-lg px-6 py-7">
       {/* Adults */}
@@ -8,9 +14,23 @@ const GuestCard = () => {
           <p className="text-gray-500">Ages 13 or above</p>
         </div>
         <div className="flex items-center gap-2">
-          <button className="px-2 border-[2px] rounded-full ">-</button>
-          <p>{5}</p>
-          <button className="px-2 border-[2px] rounded-full ">+</button>
+          <button
+            onClick={() => {
+              adults < 1
+                ? alert("You can not select below 0")
+                : setAdult(adults - 1);
+            }}
+            className="px-2 border-[2px] rounded-full "
+          >
+            -
+          </button>
+          <p>{adults}</p>
+          <button
+            onClick={() => setAdult(adults + 1)}
+            className="px-2 border-[2px] rounded-full "
+          >
+            +
+          </button>
         </div>
       </div>
       {/* children */}
@@ -20,9 +40,23 @@ const GuestCard = () => {
           <p className="text-gray-500">Ages 2–12</p>
         </div>
         <div className="flex items-center gap-2">
-          <button className="px-2 border-[2px] rounded-full ">-</button>
-          <p>{5}</p>
-          <button className="px-2 border-[2px] rounded-full ">+</button>
+          <button
+            onClick={() => {
+              children < 1
+                ? alert("You can not select below 0")
+                : setChildren(children - 1);
+            }}
+            className="px-2 border-[2px] rounded-full "
+          >
+            -
+          </button>
+          <p>{children}</p>
+          <button
+            onClick={() => setChildren(children + 1)}
+            className="px-2 border-[2px] rounded-full "
+          >
+            +
+          </button>
         </div>
       </div>
       {/* Infants */}
@@ -32,9 +66,23 @@ const GuestCard = () => {
           <p className="text-gray-500">Under 2</p>
         </div>
         <div className="flex items-center gap-2">
-          <button className="px-2 border-[2px] rounded-full ">-</button>
-          <p>{5}</p>
-          <button className="px-2 border-[2px] rounded-full ">+</button>
+          <button
+            onClick={() => {
+              infants < 1
+                ? alert("You can not select below 0")
+                : setInfants(infants - 1);
+            }}
+            className="px-2 border-[2px] rounded-full "
+          >
+            -
+          </button>
+          <p>{infants}</p>
+          <button
+            onClick={() => setInfants(infants + 1)}
+            className="px-2 border-[2px] rounded-full "
+          >
+            +
+          </button>
         </div>
       </div>
     </div>
